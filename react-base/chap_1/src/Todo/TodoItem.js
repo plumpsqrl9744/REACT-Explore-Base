fe5d4ec973048d534f67bc55be721e3cff7fa66b
plumpsqrl9744 <rnwkddn1234@nate.com>
2024-01-01 20:37:10 +0900

    chap_1 blog 정리 완료

--- react-base/chap_1/src/Todo/TodoItem.js
+++ react-base/chap_1/src/Todo/TodoItem.js
@@ -1,19 +1,17 @@
-export default function TodoItem ({item,index ,setTodo}) {
-
-    const onChange = (e) => {
-
-        setTodo((prevTodo) => {
-            
-            const nextTodo = [...prevTodo]
-            nextTodo[index] = {...nextTodo[index]}
-            nextTodo[index].completed = e.target.check;
-            return nextTodo
-        })
-    }
-    return (
-        <div>
-            <input type="checkbox" checked={item.completed} onChange={onChange}/>
-            {item.title}
-        </div>
-    )
+export default function TodoItem({ item, index, setTodo }) {
+  const onChange = (e) => {
+    setTodo((prevTodo) => {
+      const nextTodo = [...prevTodo];
+      nextTodo[index] = { ...nextTodo[index] };
+      nextTodo[index].completed = e.target.check;
+      console.log(nextTodo);
+      return nextTodo;
+    });
+  };
+  return (
+    <div>
+      <input type="checkbox" checked={item.completed} onChange={onChange} />
+      {item.title}
+    </div>
+  );
 }
